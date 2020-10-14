506f82ef511d6d671cc7887b2a63e2ee2bad742b
アレチ <eliard.alexis@gmail.com>
2020-10-14 15:49:33 +0200

    Update photos.php

--- photos.php
+++ photos.php
@@ -2,6 +2,13 @@
 	foreach(parse_ini_file("config.ini") as $key => $val) {
 		define($key, $val);
 	} unset($key); unset($val);
+
+	try {
+		$db = new PDO("mysql:host=".DB['HOST'].";dbname=".DB['NAME'], DB['USER'], DB['PASSWORD']);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e){
+		echo "Erreur : " . $e->getMessage();
+	}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
