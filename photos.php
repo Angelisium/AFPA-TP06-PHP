4b043562ace78c936bb0a955f2a6cea9c6fe64a1
アレチ <eliard.alexis@gmail.com>
2020-10-14 15:56:26 +0200

    Update photos.php

--- photos.php
+++ photos.php
@@ -9,6 +9,12 @@
 	} catch(PDOException $e){
 		echo "Erreur : " . $e->getMessage();
 	}
+
+	$requête = $db->query('SHOW TABLES FROM `TP06`');
+	$TABLES = $requête->fetchAll();
+	$requête->closeCursor();
+
+	$table = $_GET['table'] ? $_GET['table'] : NULL;
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -17,5 +23,10 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Photo</title>
 	</head>
-	<body></body>
+	<body>
+<?php foreach($TABLES as $k => $v) { ?>
+		<a href="?table=<?=$k?>"><?=$v[0]?></a>
+<?php } ?>
+	<!-- is_null -->
+	</body>
 </html>
