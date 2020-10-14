c03cfdd509ea4ff9c76e758d974120f439c71709
アレチ <eliard.alexis@gmail.com>
2020-10-14 15:48:29 +0200

    Update photos.php

--- photos.php
+++ photos.php
@@ -1,3 +1,8 @@
+<?php
+	foreach(parse_ini_file("config.ini") as $key => $val) {
+		define($key, $val);
+	} unset($key); unset($val);
+?>
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
@@ -5,7 +10,5 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Photo</title>
 	</head>
-	<body>
-		
-	</body>
+	<body></body>
 </html>
