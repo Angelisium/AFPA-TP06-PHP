55e8b85361bb87d028ae7d7d0e3c3e0cb798e439
アレチ <eliard.alexis@gmail.com>
2020-10-14 14:16:30 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -8,11 +8,12 @@ CREATE TABLE `TP06`.`mes_categorie` (
 CREATE TABLE `TP06`.`mes_photos` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`nom_photo` VARCHAR(20) BINARY NOT NULL,
-	`categorie` INT UNSIGNED NOT NULL,
+	`categorie` INT NOT NULL,
 	`titre` VARCHAR(50) BINARY,
 	`hauteur` SMALLINT NOT NULL,
 	`largeur` SMALLINT NOT NULL,
 	`date` DATE DEFAULT '1000-01-01',
 	`liste_mots` VARCHAR(100),
-	PRIMARY KEY (`id`)
+	PRIMARY KEY (`id`),
+	CONSTRAINT `in_categorie` FOREIGN KEY (`categorie`) REFERENCES mes_categorie(id)
 ) ENGINE = InnoDB;
